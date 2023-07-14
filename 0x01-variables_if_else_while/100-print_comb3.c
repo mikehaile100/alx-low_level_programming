a0941b28fbb53b61950ca746e255d1848b15b292
Michias Hailemichael <mikehaile100@gmail.com>
2023-07-14 15:27:28 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main - prog prints all possible d/t combo of two digits
+*Description: 'prints combo of numbers'
+*return: 0 (success)
+*/
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+
+	putchar('\n');
+	return (0);
+}
