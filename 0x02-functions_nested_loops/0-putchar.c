35113f6032ea566f68551fbfd80fb023bd2931d7
Michias Hailemichael <mikehaile100@gmail.com>
2023-07-19 12:23:18 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: 'prints putchar to stdout'
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
